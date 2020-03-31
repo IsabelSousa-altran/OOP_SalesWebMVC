6a54351abddb021d115f907e76eeccfd19466284
Isabel Sousa <belinhasousa.1@gmail.com>
2020-03-31 18:10:51 +0100

    Update seller and custom service exception

--- SalesWebMVC/SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/SalesWebMVC/Services/SellerService.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using SalesWebMVC.Data;
 using SalesWebMVC.Models;
 using Microsoft.EntityFrameworkCore;
+using SalesWebMVC.Services.Exceptions;
 
 namespace SalesWebMVC.Services
 {
@@ -46,6 +47,30 @@ namespace SalesWebMVC.Services
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+
+        public void Update(Seller seller)
+        {
+            // Only update if there is already an id equal to the one provided
+            if (!_context.Seller.Any(x => x.Id == seller.Id))
+            {
+                throw new NotFoundException("Id not found");
+            }
+            try
+            {
+                _context.Update(seller);
+                _context.SaveChanges();
+            }
+            // I am intercepting an exception from the data access level 
+            // and re-launching that exception but at the service level.
+            // Very important to segregate the layers.
+            // The controller(SellersControler) will only have to deal with exceptions from the service layer
+            catch (DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
+
+           
         }
     }
 }
