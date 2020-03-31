a426811e35a58b7cf1f9803908cae59081e5f86a
Isabel Sousa <belinhasousa.1@gmail.com>
2020-03-31 17:02:31 +0100

    Delete Seller

--- SalesWebMVC/SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/SalesWebMVC/Controllers/SellersController.cs
@@ -47,5 +47,29 @@ namespace SalesWebMVC.Controllers
             // It will redirect to the sellers page
             return RedirectToAction(nameof(Index));
         }
+
+        // ? - optional
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
